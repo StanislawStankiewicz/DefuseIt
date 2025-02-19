3ec5f9ae575b96c80c2f60cce3b7f7302ad0a2ba
Stanislaw <s.stankievitz@gmail.com>
2025-02-20 00:27:09 +0100

    add Cables

---
+++ Cables/cables/cables.ino
@@ -0,0 +1,65 @@
+#define DATA_PIN 9
+#define LATCH_PIN 10
+#define CLOCK_PIN 11
+#define GREEN_LED 12
+#define RED_LED 13
+
+const int inputPins[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+uint8_t activePattern = 0;
+
+void setup() {
+  Serial.begin(115200);
+    pinMode(DATA_PIN, OUTPUT);
+    pinMode(LATCH_PIN, OUTPUT);
+    pinMode(CLOCK_PIN, OUTPUT);
+    pinMode(GREEN_LED, OUTPUT);
+    pinMode(RED_LED, OUTPUT);
+
+    // Set input pins with pull-ups
+    for (int i = 0; i < 8; i++) {
+        pinMode(inputPins[i], INPUT_PULLUP);
+    }
+
+    randomSeed(analogRead(A1));
+
+    // Generate and display pattern once
+    activePattern = random(1, 256);
+    Serial.println(activePattern);
+    digitalWrite(LATCH_PIN, LOW);
+    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, activePattern);
+    digitalWrite(LATCH_PIN, HIGH);
+}
+
+void loop() {
+    bool correct = true;
+    bool anyWrong = false;
+
+    for (int i = 0; i < 8; i++) {
+        bool shouldBeUnplugged = (activePattern >> i) & 1;
+        bool isUnplugged = digitalRead(inputPins[i]) == HIGH;
+
+        if (shouldBeUnplugged && !isUnplugged) {
+            correct = false;
+        }
+        if (!shouldBeUnplugged && isUnplugged) {
+            anyWrong = true;
+        }
+    }
+
+    if (correct && !anyWrong) {
+        shutdownAndFlash(GREEN_LED);
+    } else if (anyWrong) {
+        shutdownAndFlash(RED_LED);
+    }
+}
+
+void shutdownAndFlash(int ledPin) {
+    // Turn off all outputs
+    digitalWrite(LATCH_PIN, LOW);
+    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, 0);
+    digitalWrite(LATCH_PIN, HIGH);
+
+    digitalWrite(ledPin, HIGH);
+
+    while (true);  // Halt system
+}
